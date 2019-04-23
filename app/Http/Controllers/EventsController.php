7f3c3616c37512fc316024d6cebe4b69bcbe9600
spencerhuston <sjh91910@gmail.com>
2019-04-23 19:28:58 -0400

    Events added to database

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Auth;
 use Validator;
-use App\Event;
+use App\Events;
 
 class EventsController extends Controller
 {
@@ -33,22 +35,48 @@ class EventsController extends Controller
         $validator = Validator::make($request->all(),[
             'event_name' => 'required',
             'start_date' => 'required',
-            'end_date' => 'required'
+	    'end_date' => 'required',
+	    'start_time' => 'required',
+	    'end_time' => 'required'
         ]);
 
         if($validator->fails()){
             \Session::flash('warning', 'Please enter the valid details');
             return Redirect::to('/events')->withInput()->withErrors($validator);
-
         }
 
-        $event = new Event;
-        $event->event_name = $request['event_name'];
-        $event->start_date = $request['start_date'];
-        $event->end_date = $request['end_date'];
-        $event->save();
+	/*
+	$event = new Events;
+	$event->name = $request['event_name'];
+	$event->start_date = $request['start_date'];
+	$event->end_date = $request['end_date'];
+	$event->start_time = $request['start_time'];
+	$event->end_time = $request['end_time'];
+	$event->email = Auth::user()->email;
+	 */
 
-        \Session::flash('succes', 'Event has been added to your calendar.');
+	DB::table('events')->insertGetId(['name' => $request['event_name'],
+					  'start_date' => $request['start_date'],
+				  	  'end_date' => $request['end_date'],
+					  'start_time' => $request['start_time'],
+					  'end_time' => $request['end_time'],
+					  'email' => Auth::user()->email,
+				        ]);
+
+        \Session::flash('success', 'Event has been added to your calendar.');
         return Redirect::to('/events');
     }
+
+    /*
+    protected function create(Request $request)
+    {
+	return new App\Event::create([
+		'name' => $request['event_name'],
+		'start_date' => $request['start_date'],
+		'end_date' => $request['end_date'],
+		'start_time' => $request['start_time'],
+		'end_time' => $request['end_time'],
+		'email' => Auth::user()->email,
+	]);
+    }*/
 }
