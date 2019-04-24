b94caec122ad220653f55a8c83816a98a9a773f7
spencerhuston <sjh91910@gmail.com>
2019-04-24 00:54:22 -0400

    Events working on calendar

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Auth;
 use Validator;
@@ -16,8 +17,25 @@ class CalendarController extends Controller
     //shows the events on the page
     public function index(){
 
-        $event_list = [];
-        $calendar = Calendar::addEvents($event_list);
+	$events = DB::table('events')->where('email',Auth::user()->email)->get();
+	$events_list = [];
+	
+	foreach ($events as $event) {
+		
+		if (strlen($event->name) > 30)
+		$event->name = substr($event->name, 0, 20)."\n".substr($event->name, 21, strlen($event->name) - 20);
+	
+	$desc = $event->name."\nfrom ".$event->start_time." to ".$event->end_time;
+
+		$event_list[] = Calendar::event(
+			$desc,
+			true,
+			$event->start_date,
+			$event->end_date
+		);
+	}
+
+	$calendar = Calendar::addEvents($event_list);
         return view('calendar', compact('calendar'));
     }
 
